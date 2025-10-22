408e6f6d4356083fe7cabefa630d7cf390d222b9
Jaden <jaden.kinney@gmail.com>
2025-10-21 21:58:09 -0700

    Fix pubspec name and test file for RangeBuddy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,10 +6,10 @@ import 'package:rangebuddy/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RangeBuddyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('Press Generate to start!'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
@@ -24,7 +17,7 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    expect(find.text('Press Generate to start!'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
